c38aef96044999ac83ac069df2cd21f584727559
Johnathan Normand <jnormand10@gmail.com>
2022-01-08 11:08:10 -0600

    added files and html mockup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 5000;
+const todoRouter = require('./routes/todo.router');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+//ROUTES
+app.use('/todo', todoRouter);
+
+//Start listening for requests
+app.listen(PORT, () => {
+    console.log('listening on PORT', PORT);
+    
+})
+
